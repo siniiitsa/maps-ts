80e4cecde3f3d111e312f3b11e1a8cae29c395ea
Sergey <siniiitsa@gmail.com>
2021-01-07 18:43:44 +0200

    add CustomMap class

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 import { User } from './User';
 import { Company } from './Company';
+import { CustomMap } from './CustomMap';
 
 const user = new User();
 const company = new Company();
 
-console.log(user, company);
+const map = new CustomMap('map');
+map.addMarker(user);
+map.addMarker(company);
